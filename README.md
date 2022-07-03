8d497449c29126ef072a86d58a6d5d76b1afc7e6
Pritesh Patel <contact@pritesh9.com>
2022-07-03 06:39:49 -0400

    Parcel namer workes, instrunctons updated

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# parcel-namer-preserve-structure
-Preserves file names and dir structure.
+# @pritesh9/parcel-namer-preserve-structure
+
+Preserves file names and directory structure.
+Only kicks in production mode. Developement mode is skiped as it requires unique names.
+Content hashing is disabled.
+
+Add "sourceFolder" section in package.json and specify source directory relative to current working directory.
+Example: "sourceFolder": "src"
+(this line species, all source files are located in "src" folder in current working directoy.)
+
+Add following to .parcelrc file located in project root directory (next to package.json)
+{
+	"extends": "@parcel/config-default",
+	"namers": ["@pritesh9/parcel-namer-preserve-structure", "..."]
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+const { Namer } = require("@parcel/plugin");
+const path = require("path");
+const fs = require("fs");
+
+const SOURCE_FOLDER = "sourceFolder";
+
+module.exports = new Namer({
+	name({ bundle, options }) {
+
+		if (options.mode === "production") {
+
+			const packageJson = fs.readFileSync(path.join(process.cwd(), 'package.json')).toString();
+			const packageInfo = JSON.parse(packageJson);
+			const srcDir = packageInfo[SOURCE_FOLDER];
+
+			if (!srcDir) {
+				logger.warn({
+					message: `no sourceFolder section in package.json.`
+				});
+				return null;
+			}
+
+			let filePath = bundle.getMainEntry().filePath;
+
+			let distName = basenameWithoutExtension(filePath);
+			let distPath = path.dirname(`${path.relative(path.join(process.cwd(), srcDir), filePath)}`);
+
+			// if (!bundle.needsStableName) {
+			// 	distName += "." + bundle.hashReference;
+			// }
+
+			console.log("ENTRY: " + filePath);
+
+			if (distPath === ".") {
+				console.log("NAME: " + distName + '.' + bundle.type);
+				return distName + '.' + bundle.type;
+			}
+			
+			console.log("NAME: " + distPath + '\\' + distName + '.' + bundle.type);
+			return distPath + '\\' + distName + '.' + bundle.type;
+		}
+
+		// This namer handles all files but just in case...
+		// Allow the next namer to handle this bundle.
+		return null;
+	}
+});
+
+function basenameWithoutExtension(file) {
+	return path.basename(file, path.extname(file));
+}
